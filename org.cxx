d3153b97980ef9f9941c1452d408dcfbe5be13bb
schatt89 <95schatt@gmail.com>
2016-12-08 17:39:13 +0300

    Done!

--- org.cxx
+++ org.cxx
@@ -1,7 +1,24 @@
 #encoding "utf-8"
 
 OrgDescr -> AnyWord<kwtype="org_descr">;
+OrgHead -> AnyWord<kwtype="org_head">;
 
-Org -> OrgDescr interp(Object.Descr) AnyWord<h-reg1> interp(Object.Name; Object.Type="ORG");
+OrgDescr ->OrgDescr GP;
+OrgDescr ->Adj<gnc-agr[1]> OrgDescr<rt, gnc-agr[1]>; // rt- главное слово
+
+GP -> Word<gram="S, gen">; //S - сущ gen-род падеж
+
+
+Quoted_Words -> AnyWord<l-quoted,r-quoted>;
+Quoted_Words -> AnyWord<l-quoted,~r-quoted> Word<~l-quoted,~r-quoted>* AnyWord<~l-quoted,r-quoted>;
+
+Org -> OrgDescr interp(Object.Descr) Word<h-reg1> interp(Object.Name; Object.Type="ORG");
+Org -> OrgDescr interp(Object.Descr) Quoted_Words interp(Object.Name; Object.Type="ORG");
+Org -> OrgDescr interp(Object.Name; Object.Type="ORG");
+
+Org_noninterp -> OrgHead AnyWord<h-reg1>;
+
+
+Org -> Org_noninterp interp(Object.Name; Object.Type="ORG");
 
 Root -> Org;
